2bd46c1b0d78d49bc22e2a837f73c08de90661c5
samervalente <samervalente@gmail.com>
2023-08-24 11:21:45 -0300

    add branding and mock data

---
+++ src/config/branding.ts
@@ -0,0 +1,8 @@
+const branding = {
+  appName: "StartFit",
+  slug: "Organize sua dieta",
+  logoUrl: "/logo-complete.png",
+  poweredBy: "Lothus Software",
+};
+
+export default branding;
---
+++ src/config/mock.ts
@@ -0,0 +1,7 @@
+export const user_data = {
+  firstname: "Samer",
+  surname: "Valente",
+  weigth: 62,
+  heigth: 180,
+  IMC: 18.5,
+};
